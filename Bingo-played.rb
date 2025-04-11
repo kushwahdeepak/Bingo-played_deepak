cd49a2fe93b110d6902455838caad1a732592d82
deepak <deepakkushwahsk@gmail.com>
2025-04-11 18:52:17 +0530

    create method distribute remaining numbers

--- Bingo-played.rb
+++ Bingo-played.rb
@@ -15,15 +15,28 @@ class HousieTicket
   # Generates a 3x9 ticket by initializing columns
   def initialize
     @column_counts = Array.new(9, 1)
+    allocate_extra_numbers
     @columns = generate_columns
     @ticket = Array.new(3) { Array.new(9, 'X') }
     allocate_to_rows
     finalize_rows
   end
 
+#  Picks actual numbers for each column with unique numbers
   def generate_columns
     @column_counts.map.with_index do |count, i|
       COLUMN_RANGES[i].sample(count).sort
+    end
+  end
+
+  def allocate_extra_numbers
+    remaining = 15 - @column_counts.sum
+    while remaining.positive?
+      index = rand(9)
+      if @column_counts[index] < 3
+        @column_counts[index] += 1
+        remaining -= 1
+      end
     end
   end
 
@@ -36,6 +49,7 @@ class HousieTicket
     end
   end
 
+#  assigning numbers
   def finalize_rows
     @ticket.each_with_index do |row, idx|
         trim_row(row)
