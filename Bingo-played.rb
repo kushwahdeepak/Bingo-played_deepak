1cad8a5d9409aee41a13ddf4e2759745531e9976
deepak <deepakkushwahsk@gmail.com>
2025-04-11 19:25:34 +0530

    done

--- Bingo-played.rb
+++ Bingo-played.rb
@@ -87,3 +87,10 @@ end
 ticket = HousieTicket.new
 ticket.print
 
+
+# Each ticket shows exactly 15 numbers, 
+# ticket every number from 1 to 90 is used exactly once.
+# A ticket is a grid of 3 rows by 9 columns. 
+# The first column contains numbers from 1 to 9; 
+# the second column numbers from 10 to 19; the third 20 to 29 and so on up until the last column, which contains numbers from 80 to 90.
+# Each column contains one, two or three numbers, in increasing order downwards. Each row contains exactly 5 numbers (and hence 4 blanks)
